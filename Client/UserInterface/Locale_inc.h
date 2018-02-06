00bbfc5af9c27e2596abe7732d5e8dc1d176841b
Syserino <giuseppealfiero95@gmail.com>
2018-02-06 15:55:48 +0100

    First upload.

---
+++ Client/UserInterface/Locale_inc.h
@@ -0,0 +1,55 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////
+// ### Default Ymir Macros ###
+
+#define LOCALE_SERVICE_EUROPE
+#define ENABLE_COSTUME_SYSTEM
+#define ENABLE_ENERGY_SYSTEM
+#define ENABLE_DRAGON_SOUL_SYSTEM
+#define ENABLE_NEW_EQUIPMENT_SYSTEM
+
+// ### Default Ymir Macros ###
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+// ### New From LocaleInc ###
+
+#define ENABLE_PACK_GET_CHECK
+//#define ENABLE_CANSEEHIDDENTHING_FOR_GM
+#define ENABLE_PROTOSTRUCT_AUTODETECT
+
+#define ENABLE_PLAYER_PER_ACCOUNT5
+#define ENABLE_LEVEL_IN_TRADE
+#define ENABLE_DICE_SYSTEM
+#define ENABLE_EXTEND_INVEN_SYSTEM
+
+//#define ENABLE_PERFECT_WEAPONS
+//#define ENABLE_PERFECT_ARMORS
+
+#define ENABLE_LVL115_ARMOR_EFFECT
+//#define ENABLE_JIN_ARMOR_EFFECT
+//#define ENABLE_SOUL_ARMOR_EFFECT
+
+#define WJ_SHOW_MOB_INFO
+#ifdef WJ_SHOW_MOB_INFO
+	#define ENABLE_SHOW_MOBAIFLAG
+	#define ENABLE_SHOW_MOBLEVEL
+#endif
+
+// ### New From LocaleInc ###
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+// ### From GameLib ###
+
+#define ENABLE_WOLFMAN_CHARACTER
+#define ENABLE_MAGIC_REDUCTION_SYSTEM
+#define ENABLE_MOUNT_COSTUME_SYSTEM
+#define ENABLE_WEAPON_COSTUME_SYSTEM
+#define ENABLE_ACCE_COSTUME_SYSTEM
+#define ENABLE_OBJECT_SCALING
+#define ENABLE_AUTO_PVP_SYSTEM
+
+// ### From GameLib ###
+//////////////////////////////////////////////////////////////////////////
---
+++ Client/UserInterface/Version.h
@@ -0,0 +1,7 @@
+#pragma once
+// #define VER_FILE_VERSION   1,0,28249,0
+// #define VER_FILE_VERSION_STR   "1.0.28249.1"
+// int METIN2_GET_VERSION() { return 28249; }
+#define VER_FILE_VERSION   1,0,40999,0
+#define VER_FILE_VERSION_STR   "1.0.40999.1"
+int METIN2_GET_VERSION() { return 40999; }
